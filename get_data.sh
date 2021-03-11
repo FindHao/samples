c7107c21774b9a8a0b8c0432f0d1a1d874609773
FindHao <find@findhao.net>
2021-03-11 10:41:06 -0500

    update data download script

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -c https://github.com/FindHao/GVProf-samples/releases/download/0.2/data.tar.gz
+wget -c https://github.com/FindHao/samples/releases/download/0.1/data.tar.gz
 tar zxvf data.tar.gz
 rm data.tar.gz
